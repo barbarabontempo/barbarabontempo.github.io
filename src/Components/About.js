cb9049261775f80ba680792c8c5ce835d59992a0
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-12-10 19:33:17 -0500

    fixed about me

--- src/Components/About.js
+++ src/Components/About.js
@@ -34,18 +34,12 @@ class About extends Component {
             <h2>About Me</h2>
 
             <p>
-             Hello, my name is Barbara. I'm a full-stack web developer with
-             experience in Ruby, Ruby on Rails, Javascript, and
-             React. I enjoy creating things that live on the internet and my goal is to always build products that provide performant, accessible, and 'easy to use', user experiences. Before coding, I was on the track to medical school and prior to medical school, I was preparing for law school. After realizing I wasn't passionate enough to continue with those fields, I found myself learning how to code. Programming became this magical tool that allowed me to use all my knowledge from my past experiences in different fields to bring my ideas to life.
+             Hi! My name is Barbara. I'm a full-stack web developer with experience in Ruby, Ruby on Rails, Javascript, and
+             React. I love building things that live on the internet and my goal is to always create products that provide performant, accessible, and 'easy to use', user experiences. Before coding, I worked and studied in various fields including medicine, policy, and law. But ultimately, I wanted to be more creative. So I learned to code! Programming soon became this magical tool that allowed me to bring my ideas to life! It has given me the opportunity to create projects that reflect on all my past experiences. I'm alaways learning. Currently im practicing my skillsvwith MongoDB, React, Node JS, and Python.
            </p>
  
            <p>
-             I'm constantly learning new things. Currently, those things include gaining more experience with MongoDB, React, Node JS, and Python.
-           </p>
- 
-           <p>
-             When I am not coding, I find inspiration through yoga, painting,
-             bird watching, hiking, and taking photos. I am passionate about people and hope that my work can contribute to providing for those who do not have equal resource access to technology.{" "}
+             When I am not coding, I find inspiration through yoga, painting, bird watching, hiking, and taking photos. I am passionate about people and I hope that my work can contribute to providing for those who do not have equal resource access to technology. Oh and saving the world from climate change! 🌍
            </p>
 
 
