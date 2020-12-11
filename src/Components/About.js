2f740ba419786b5b4533b6c0f5a03c41055b3f46
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-12-11 15:12:59 -0500

    fixed typo

--- src/Components/About.js
+++ src/Components/About.js
@@ -35,7 +35,7 @@ class About extends Component {
 
             <p>
              Hi! My name is Barbara. I'm a full-stack web developer with experience in Ruby, Ruby on Rails, Javascript, and
-             React. I love building things that live on the internet and my goal is to always create products that provide performant, accessible, and 'easy to use', user experiences. Before coding, I worked and studied in various fields including medicine, policy, and law. But ultimately, I wanted to be more creative. So I learned to code! Programming soon became this magical tool that allowed me to bring my ideas to life! It has given me the opportunity to create projects that reflect on all my past experiences. I am constantly learning new things. Currently, I'm practicing my skills with MongoDB, React, Node JS, and Python.
+             React. I love building things that live on the internet and my goal is to always create products that provide performant, accessible, and 'easy to use', user experiences. Before coding, I worked and studied in various fields including medicine, policy, and law. But ultimately, I wanted to be more creative. So I learned to code! Programming soon became this magical tool that allowed me to bring my ideas to life! It has given me the opportunity to create projects that reflect on all my past experiences. I am also constantly learning new things. Currently, I'm practicing my skills with MongoDB, React, Node JS, and Python.
            </p>
  
            <p>
