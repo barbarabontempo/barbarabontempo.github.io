8bc08cf9bfbca9f09ed01374d483142bc6c9e42e
Barbara Bontempo <barbarabontempo94@gmail.com>
2024-04-28 22:30:54 -0400

    adding datadog rum

--- src/App.js
+++ src/App.js
@@ -12,6 +12,24 @@ import Portfolio from "./Components/Portfolio";
 import Skills from "./Components/Skills";
 import { datadogRum } from "@datadog/browser-rum";
 
+datadogRum.init({
+  applicationId: "1aa5827a-32ae-437c-8ad5-af272edeb281",
+  clientToken: "pubc580652fff48cb39f4898c685a2312bf",
+  // `site` refers to the Datadog site parameter of your organization
+  // see https://docs.datadoghq.com/getting_started/site/
+  site: "datadoghq.com",
+  service: "barbara-portfolio",
+  env: "tetst",
+  // Specify a version number to identify the deployed version of your application in Datadog
+  version: "1.0.0",
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 20,
+  trackUserInteractions: true,
+  trackResources: true,
+  trackLongTasks: true,
+  defaultPrivacyLevel: "mask-user-input",
+});
+datadogRum.startSessionReplayRecording();
 class App extends Component {
   constructor(props) {
     super(props);
@@ -22,24 +40,6 @@ class App extends Component {
 
     ReactGA.initialize("UA-110570651-1");
     ReactGA.pageview(window.location.pathname);
-
-    datadogRum.init({
-      applicationId: "1aa5827a-32ae-437c-8ad5-af272edeb281",
-      clientToken: "pubc580652fff48cb39f4898c685a2312bf",
-      // `site` refers to the Datadog site parameter of your organization
-      // see https://docs.datadoghq.com/getting_started/site/
-      site: "datadoghq.com",
-      service: "barbara-portfolio",
-      env: "tetst",
-      // Specify a version number to identify the deployed version of your application in Datadog
-      version: "1.0.0",
-      sessionSampleRate: 100,
-      sessionReplaySampleRate: 20,
-      trackUserInteractions: true,
-      trackResources: true,
-      trackLongTasks: true,
-      defaultPrivacyLevel: "mask-user-input",
-    });
   }
 
   getResumeData() {
