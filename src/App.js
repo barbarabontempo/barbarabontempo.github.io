676a1dcb295fed7741ffb33e10ea6e0e63a300db
Barbara Bontempo <barbarabontempo94@gmail.com>
2024-04-28 22:25:10 -0400

    rum round 3 lol

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Contact from "./Components/Contact";
 import Testimonials from "./Components/Testimonials";
 import Portfolio from "./Components/Portfolio";
 import Skills from "./Components/Skills";
+import { datadogRum } from "@datadog/browser-rum";
 
 class App extends Component {
   constructor(props) {
@@ -21,6 +22,24 @@ class App extends Component {
 
     ReactGA.initialize("UA-110570651-1");
     ReactGA.pageview(window.location.pathname);
+
+    datadogRum.init({
+      applicationId: "1aa5827a-32ae-437c-8ad5-af272edeb281",
+      clientToken: "pubc580652fff48cb39f4898c685a2312bf",
+      // `site` refers to the Datadog site parameter of your organization
+      // see https://docs.datadoghq.com/getting_started/site/
+      site: "datadoghq.com",
+      service: "barbara-portfolio",
+      env: "tetst",
+      // Specify a version number to identify the deployed version of your application in Datadog
+      version: "1.0.0",
+      sessionSampleRate: 100,
+      sessionReplaySampleRate: 20,
+      trackUserInteractions: true,
+      trackResources: true,
+      trackLongTasks: true,
+      defaultPrivacyLevel: "mask-user-input",
+    });
   }
 
   getResumeData() {
--- src/index.js
+++ src/index.js
@@ -4,25 +4,5 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-import { datadogRum } from "@datadog/browser-rum";
-
-datadogRum.init({
-  applicationId: "1aa5827a-32ae-437c-8ad5-af272edeb281",
-  clientToken: "pubc580652fff48cb39f4898c685a2312bf",
-  // `site` refers to the Datadog site parameter of your organization
-  // see https://docs.datadoghq.com/getting_started/site/
-  site: "datadoghq.com",
-  service: "barbara-portfolio",
-  env: "tetst",
-  // Specify a version number to identify the deployed version of your application in Datadog
-  version: "1.0.0",
-  sessionSampleRate: 100,
-  sessionReplaySampleRate: 20,
-  trackUserInteractions: true,
-  trackResources: true,
-  trackLongTasks: true,
-  defaultPrivacyLevel: "mask-user-input",
-});
-
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
